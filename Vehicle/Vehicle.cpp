009306cc70092a2886768e64fb22269f9013c4ef
Paul <PaulDCross@users.noreply.github.com>
2017-06-16 22:02:57 +0100

    Adding methods

--- Vehicle/Vehicle.cpp
+++ Vehicle/Vehicle.cpp
@@ -9,3 +9,44 @@ Vehicle::Vehicle() {
 Vehicle::~Vehicle() {
 
 }
+
+
+void Vehicle::update() {
+	// Update velocity
+	_velocity.addVector(_acceleration);
+	// Limit velocity
+	_velocity.limit(_maxSpeed);
+	// Update location
+	_location.addVector(_velocity);
+	// Reset acceleration each cycle
+	_acceleration.mulScalar(0.0);
+}
+
+void Vehicle::applyForce(Pvector v) {
+	// We could add mass here if we want A = F / M
+	_acceleration.addVector(v);
+}
+
+void Vehicle::applyWalls(){
+
+	if (_location.x < d) {
+		Pvector desired(_maxSpeed, _velocity.y);
+	}
+	else if (_location.x > width - d) {
+		Pvector desired(-_maxSpeed, _velocity.y);
+	}
+
+	if (_location.y < d) {
+		Pvector desired(_velocity.x, _maxSpeed);
+	}
+	else if (_location.y > height - d) {
+		Pvector desired(_velocity.x, -_maxSpeed);
+	}
+
+	if (desired !== null) {
+		desired.setMagnitude(_maxSpeed);
+		desired.subVector(_velocity);
+		desired.limit(_maxForce);
+		applyForce(desired);
+	}
+};
