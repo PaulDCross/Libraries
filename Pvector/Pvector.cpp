6028e7bc8c67fbe5b5e742fe6eb8889c5533a8cd
Cross <paulcross@f2s.com>
2017-12-16 15:13:56 +0000

    cpp vector files

--- Pvector/Pvector.cpp
+++ Pvector/Pvector.cpp
@@ -1,14 +1,13 @@
 #include "Pvector.h"
+#include <math.h> 
 
 // Pvector function definitions from Pvector.h //
 
-// Sets the x and y values for Pvector
 void Pvector::set(float i, float j) {
 	x = i;
 	y = j;
 }
 
-// Adds the x and y components of the vector object used as the parameter to the components in this object
 void Pvector::addVector(Pvector v) {
 	x += v.x;
 	y += v.y;
@@ -19,7 +18,6 @@ void Pvector::addScalar(float s) {
 	y += s;
 }
 
-// Subtracts the x and y components of the vector object used as the parameter from the components in this object
 void Pvector::subVector(Pvector v) {
 	x -= v.x;
 	y -= v.y;
@@ -32,61 +30,51 @@ Pvector Pvector::subTwoVector(Pvector v1, Pvector v2) {
 	return tmp;
 }
 
-// Subtracts from the Pvector a constant number
 void Pvector::subScalar(float s) {
 	x -= s;
 	y -= s;
 }
 
-// Multiplies two vectors
 void Pvector::mulVector(Pvector v) {
 	x *= v.x;
 	y *= v.y;
 }
 
-// Multiplies the vector by a constant number
 void Pvector::mulScalar(float s) {
 	x *= s;
 	y *= s;
 }
 
-// Divides two vectors
 void Pvector::divVector(Pvector v) {
 	x /= v.x;
 	y /= v.y;
 }
 
-// Divides the vector by a constant value
 void Pvector::divScalar(float s) {
 	x /= s;
 	y /= s;
 }
 
-// Limits the vector to a given magnitude
 void Pvector::limit(float s) {
 	normalise();
 	mulScalar(s);
 }
 
-// Calculates the distance between the first Pvector and the second Pvector
 float Pvector::distance(Pvector v) const {
-	float dx = x - v.x;
-	float dy = y - v.y;
-	float dist = sqrt((dx*dx) + (dy*dy));
+	double dx = x - v.x;
+	double dy = y - v.y;
+	double dist = sqrt((dx*dx) + (dy*dy));
 	return dist;
 }
 
-// Calculates the dot product between vector 1 and vector 2
 float Pvector::dotProduct(Pvector v) const {
 	return (x*v.x) + (y*v.y);
 }
 
-// Calculates the magnitude of the vector
 float Pvector::magnitude() const {
 	return sqrt((x*x) + (y*y));
 }
 
-// Calculates the angle between the first and second vectors
 float Pvector::angleBetween(Pvector v) const {
 	if ((x == 0 && y == 0) || (v.x == 0 && v.y == 0))
 		return 0.0;
@@ -99,7 +87,6 @@ float Pvector::angleBetween(Pvector v) const {
 	return acos(amt);
 }
 
-// Normalise divides x and y by magnitude if it has a magnitude
 void Pvector::normalise() {
 	float m = magnitude();
 	if (m > 0)
@@ -108,14 +95,18 @@ void Pvector::normalise() {
 		set(x, y);
 }
 
-float Pvector::getAngle() const {
+float Pvector::getAngleRad() const {
 	float a = atan2(x, y);
 	a = a*(a >= 0) + (a + 2 * M_PI)*(a < 0);
 	return a;
 }
 
+unsigned int Pvector::getAngleDeg() const {
+	float a = atan2(x, y);
+	a = a*(a >= 0) + (a + 2 * M_PI)*(a < 0);
+	return unsigned int(round(a*(180 / M_PI)));
+}
 
-// Creates and returns a copy of the Pvector used as a parameter
 Pvector Pvector::copy(Pvector v) {
 	Pvector copy(v.x, v.y);
 	return copy;
--- Pvector/Pvector.h
+++ Pvector/Pvector.h
@@ -1,9 +1,8 @@
 
-#ifndef PVECTOR_H
-#define PVECTOR_H
-#define _USE_MATH_DEFINES
-#include <math.h> 
+#ifndef Pvector_h__
+#define Pvector_h__
 
+#define _USE_MATH_DEFINES
 
 class Pvector {
 public:
@@ -14,44 +13,219 @@ public:
 		x = 0;
 		y = 0;
 	}
-
 	Pvector(float xCoordinate, float yCoordinate) {
 		x = xCoordinate;
 		y = yCoordinate;
 	}
-
 	~Pvector() {}
 
 	// Mutator Functions
 
-	void set(float, float);
+	//************************************
+	// Method:    set
+	// FullName:  Pvector::set
+	// Access:    public 
+	// Returns:   void
+	// Qualifier:
+	// Parameter: float i
+	// Parameter: float j
+	// Sets the x and y values for Pvector
+	//************************************
+	void set(float i, float j);
 
-	// Adds the x and y components of the vector object used as the parameter to the components in this object
-	void addVector(Pvector);
-	void addScalar(float);
+	//************************************
+	// Method:    addVector
+	// FullName:  Pvector::addVector
+	// Access:    public 
+	// Returns:   void
+	// Qualifier:
+	// Parameter: Pvector v
+	// Adds the x and y components of the Pvector object used as the parameter to this object's x and y components.
+	//************************************
+	void addVector(Pvector v);
 
-	// Subtracts the x and y components of the vector object used as the parameter from the components in this object
-	void subVector(Pvector);
-	static Pvector subTwoVector(Pvector, Pvector);
-	void subScalar(float);
+	//************************************
+	// Method:    addScalar
+	// FullName:  Pvector::addScalar
+	// Access:    public 
+	// Returns:   void
+	// Qualifier:
+	// Parameter: float s
+	// Adds the scalar parameter to the x and y components of this Pvector object.
+	//************************************
+	void addScalar(float s);
 
-	void mulVector(Pvector);
-	void mulScalar(float);
+	//************************************
+	// Method:    subVector
+	// FullName:  Pvector::subVector
+	// Access:    public 
+	// Returns:   void
+	// Qualifier:
+	// Parameter: Pvector v
+	// Subtracts the x and y components of the Pvector object used as the parameter to this object's x and y components.
+	//************************************
+	void subVector(Pvector v);
 
-	void divVector(Pvector);
-	void divScalar(float);
+	//************************************
+	// Method:    subTwoVector
+	// FullName:  Pvector::subTwoVector
+	// Access:    public static 
+	// Returns:   Pvector
+	// Qualifier:
+	// Parameter: Pvector v1
+	// Parameter: Pvector v2
+	// Takes two Pvector objects as parameters and subtracts their corresponding x and y values.
+	//************************************
+	static Pvector subTwoVector(Pvector v1, Pvector v2);
 
-	void limit(float);
+	//************************************
+	// Method:    subScalar
+	// FullName:  Pvector::subScalar
+	// Access:    public 
+	// Returns:   void
+	// Qualifier:
+	// Parameter: float s
+	// Subtracts the scalar parameter from both x and y values of this Pvector object.
+	//************************************
+	void subScalar(float s);
 
-	// Calculating functions
-	float distance(Pvector) const;
-	float dotProduct(Pvector) const;
+	//************************************
+	// Method:    mulVector
+	// FullName:  Pvector::mulVector
+	// Access:    public 
+	// Returns:   void
+	// Qualifier:
+	// Parameter: Pvector v
+	// Multiplies the x and y components of this Pvector object by the corresponding components of the vector parameter.
+	//************************************
+	void mulVector(Pvector v);
+
+	//************************************
+	// Method:    mulScalar
+	// FullName:  Pvector::mulScalar
+	// Access:    public 
+	// Returns:   void
+	// Qualifier:
+	// Parameter: float s
+	// Multiplies the x and y components of this Pvector object by the scalar parameter.
+	//************************************
+	void mulScalar(float s);
+
+	//************************************
+	// Method:    divVector
+	// FullName:  Pvector::divVector
+	// Access:    public 
+	// Returns:   void
+	// Qualifier:
+	// Parameter: Pvector v
+	// Divides the x and y components of this Pvector object by the corresponding values of the vector parameter.
+	//************************************
+	void divVector(Pvector v);
+
+	//************************************
+	// Method:    divScalar
+	// FullName:  Pvector::divScalar
+	// Access:    public 
+	// Returns:   void
+	// Qualifier:
+	// Parameter: float s
+	// Divides the x and y components of this Pvector object by the scalar parameter.
+	//************************************
+	void divScalar(float s);
+
+	//************************************
+	// Method:    limit
+	// FullName:  Pvector::limit
+	// Access:    public 
+	// Returns:   void
+	// Qualifier:
+	// Parameter: float s
+	// Limits the magnitude of this Pvector object to the value of the parameter parsed.
+	//************************************
+	void limit(float s);
+
+	//************************************
+	// Method:    distance
+	// FullName:  Pvector::distance
+	// Access:    public 
+	// Returns:   float
+	// Qualifier: const
+	// Parameter: Pvector v
+	// Calculates the distance between this Pvector object and the Pvector object parsed.
+	//************************************
+	float distance(Pvector v) const;
+
+	//************************************
+	// Method:    dotProduct
+	// FullName:  Pvector::dotProduct
+	// Access:    public 
+	// Returns:   float
+	// Qualifier: const
+	// Parameter: Pvector v
+	// Calculates the dot product between this Pvector object and the Pvector object parsed.
+	//************************************
+	float dotProduct(Pvector v) const;
+
+	//************************************
+	// Method:    magnitude
+	// FullName:  Pvector::magnitude
+	// Access:    public 
+	// Returns:   float
+	// Qualifier: const
+	// Calculates the magnitude of this Pvector object.
+	//************************************
 	float magnitude() const;
-	float angleBetween(Pvector) const;
+
+	//************************************
+	// Method:    angleBetween
+	// FullName:  Pvector::angleBetween
+	// Access:    public 
+	// Returns:   float
+	// Qualifier: const
+	// Parameter: Pvector v
+	// Calculates the angle between the vector of this Pvector object and the vector of the Pvector object used as the parameter.
+	//************************************
+	float angleBetween(Pvector v) const;
+
+	//************************************
+	// Method:    normalise
+	// FullName:  Pvector::normalise
+	// Access:    public 
+	// Returns:   void
+	// Qualifier:
+	// Divides the x and y components of this Pvector object by it's magnitude if it has a magnitude.
+	//************************************
 	void normalise();
-	float getAngle() const;
 
-	static Pvector copy(Pvector);
+	//************************************
+	// Method:    getAngleRad
+	// FullName:  Pvector::getAngleRad
+	// Access:    public 
+	// Returns:   float
+	// Qualifier: const
+	// Returns the angle between the vertical axis and the vector of this Pvector object in radians.
+	//************************************
+	float getAngleRad() const;
+
+	//************************************
+	// Method:    getAngleDeg
+	// FullName:  Pvector::getAngleDeg
+	// Access:    public 
+	// Returns:   unsigned int
+	// Qualifier: const
+	// Returns the angle between the vertical axis and the vector of the Pvector object in degrees.
+	//************************************
+	unsigned int getAngleDeg() const;
+
+	//************************************
+	// Method:    copy
+	// FullName:  Pvector::copy
+	// Access:    public static 
+	// Returns:   Pvector
+	// Qualifier:
+	// Parameter: Pvector v
+	// Creates and returns a copy of the Pvector used as a parameter
+	//************************************
+	static Pvector copy(Pvector v);
 };
-
-#endif //!PVECTOR_H
+#endif // Pvector_h__
